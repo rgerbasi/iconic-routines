8f5855f1dfd4d40906e2e6a091724cf4ebcdd6be
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2018-05-21 09:42:15 -0400

    finally get teh list to work, fixed small typo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Project 2, iconic stunt sequences
+
+Homweork day 4 make delete button work, from array too
+
+make the fav button do something, change the style of teh li in some way
+
+###Bonus credit
+    make fav button also chang eht eobject in the array - example: 
+    ```js
+    {
+        id: 1,
+        name: 'Gi Joe',
+        fav: true,
+    }
+
+### super mega bonus
+    add buttons to move the flick/item up and down the list 
+    make the buttons also change their place in teh array
+
+
+### hyper fighting
+    allow users to edit the names of the items in teh lsit after they're added 
+    make hte span's content editable --google
+    
--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 
+
 const app = {
-    init: function(formSelector){
+    init: function(selectors){
         this.max = 0;
+        this.routines = [];
+        this.list = document.querySelector(selectors.listSelector);
+        this.template = document.querySelector(selectors.templateSelector)
 
         document
-            .querySelector(formSelector)
+            .querySelector(selectors.formSelector)
             .addEventListener('submit', (ev) => {
                 ev.preventDefault();
                 this.handleSubmit(ev);
             });
     },
 
+    removeFlick(ev){
+
+    },
+
+    renderListItem(routine){
+        const listItem = this.template.cloneNode(true)
+        listItem.classList.remove('template');
+        listItem.dataset.id = routine.id;
+        listItem
+            .querySelector('.team')
+            .textContent = routine.team;
+        
+            
+        listItem
+            .querySelector('.remove.button')
+            .addEventListener('click', this.removeFlick)
+
+        return listItem;
+    },
+
     handleSubmit: function(ev){
         ev.preventDefault();
         const f = ev.target;
-        const team = {
+        const routine = {
             id: ++this.max,
-            name: f.name.value,
+            team: f.team.value,
+            year: f.year.value,
 
         }
-        console.log(flick)
+
+        this.routines.push(routine);
+
+        const listItem = this.renderListItem(routine);
+        this.list.appendChild(listItem);
+        console.log(routine)
+        
         f.reset();
     },
 }
 
-app.init('#teamInput')
+app.init({
+    formSelector: '#teamForm',
+    listSelector: '#routineList',
+    templateSelector: '.routine.template',
+});
